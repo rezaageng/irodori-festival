ab657572d5223d8276aab75000103fc83886355a
Reza Ageng <rezaageng5@gmail.com>
2022-05-14 13:40:04 +0700

    feat(config): :wrench: background color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'irodori-primary': '#F2DDB6',
+      },
+    },
   },
   plugins: [],
 };
