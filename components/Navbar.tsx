3e885e4e43ecfe317a2f607c90e3881b0e7179bf
Reza Ageng <rezaageng5@gmail.com>
2022-05-16 15:45:48 +0700

    feat(navbar): :sparkles: add auto hide navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,64 @@
+import { useSpring, animated } from '@react-spring/web';
+import { useMediaQuery } from 'react-responsive';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { useEffect, useRef, useState } from 'react';
 
 const Navbar = () => {
   const router = useRouter();
+  const [show, setShow] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const ref = useRef(null);
+
+  const isMobile = useMediaQuery({ query: '(max-width: 640px)' });
+
+  const navAnimatedMobile = useSpring({
+    from: { bottom: show ? '-100px' : '0px', top: 'auto' },
+    to: { bottom: show ? '0px' : '-100px' },
+    immediate: !ref.current,
+  });
+  const navAnimated = useSpring({
+    from: { top: show ? '-100px' : '0px', bottom: 'auto' },
+    to: { top: show ? '0px' : '-100px' },
+    immediate: !ref.current,
+  });
+
+  const controlNavbar = () => {
+    if (typeof window !== 'undefined') {
+      if (window.scrollY > lastScrollY) setShow(false);
+      else setShow(true);
+
+      setLastScrollY(window.scrollY);
+    }
+  };
+
+  // eslint-disable-next-line consistent-return
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', controlNavbar);
+
+      return () => window.removeEventListener('scroll', controlNavbar);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [lastScrollY]);
 
   return (
-    <nav className="bg-irodori-primary fixed bottom-0 left-0 w-full">
-      <div className="flex gap-4 justify-around p-2">
+    <animated.nav
+      ref={ref}
+      style={isMobile ? navAnimatedMobile : navAnimated}
+      className="bg-irodori-primary fixed left-0 w-full sm:bottom-auto px-6 flex transition ease-in-out duration-500`"
+    >
+      <div className="hidden sm:flex flex-1">
+        <Image
+          src="/assets/logos/zephyr.png"
+          alt="zephyr"
+          width={150}
+          height={53}
+          priority
+        />
+      </div>
+      <div className="flex gap-4 justify-around sm:justify-end p-2 w-full flex-1">
         <Link href="/" passHref>
           <div className="nav-wrapper">
             <Image
@@ -20,7 +71,7 @@ const Navbar = () => {
               width={24}
               height={24}
             />
-            <span className="hidden">Home</span>
+            <span className="hidden sm:inline">Home</span>
           </div>
         </Link>
         <Link href="/gallery" passHref>
@@ -35,7 +86,7 @@ const Navbar = () => {
               width={24}
               height={24}
             />
-            <span className="hidden">Gallery</span>
+            <span className="hidden sm:inline">Gallery</span>
           </div>
         </Link>
         <Link href="/art-sources" passHref>
@@ -50,11 +101,11 @@ const Navbar = () => {
               width={24}
               height={24}
             />
-            <span className="hidden">Art Sources</span>
+            <span className="hidden sm:inline">Art Sources</span>
           </div>
         </Link>
       </div>
-    </nav>
+    </animated.nav>
   );
 };
 
