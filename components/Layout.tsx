63a93e3ed378af6ae3875b9a5c6a93d9f82e7e29
Reza Ageng <rezaageng5@gmail.com>
2022-05-17 13:22:53 +0700

    refactor(layout): :recycle: refactor layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,12 +1,8 @@
 import { FC } from 'react';
-import Navbar from './Navbar';
 
 const Layout: FC<any> = ({ children }) => (
   <div className="min-h-screen  text-white">
-    <Navbar />
-    <div className="max-w-7xl p-4 xl:m-auto">
-      <main>{children}</main>
-    </div>
+    <main>{children}</main>
   </div>
 );
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,11 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import Layout from '../components/Layout';
+import Navbar from '../components/Navbar';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <Layout>
+    <Navbar />
     <Component {...pageProps} />
   </Layout>
 );
