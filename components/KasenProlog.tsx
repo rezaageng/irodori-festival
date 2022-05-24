b02fce21845fd7bedabdee2342a339609b807288
Reza Ageng <rezaageng5@gmail.com>
2022-05-24 11:36:01 +0700

    style(home): :lipstick: remove parallax

--- components/KasenProlog.tsx
+++ components/KasenProlog.tsx
@@ -1,22 +1,10 @@
-import { ParallaxBanner } from 'react-scroll-parallax';
-
 const KasenProlog = () => (
-  <ParallaxBanner
-    layers={[
-      {
-        image: '/assets/kasen/bg.png',
-        speed: -20,
-      },
-    ]}
-    className="aspect-video md:aspect-[36/9]"
-  >
-    <div className="absolute inset-0 flex items-center justify-center">
-      <p className="text-center text-irodori-tertiary p-10 sm:p-4 lg:p-16">
-        Long ago, Inazuma had five legendary poets, people bestowed upon them
-        the title of The Five Kasen
-      </p>
-    </div>
-  </ParallaxBanner>
+  <div className="bg-[url('/assets/kasen/bg.png')] bg-banner md:bg-cover bg-center flex items-center justify-center aspect-video md:aspect-[36/9]">
+    <p className="text-center text-irodori-tertiary p-10 sm:p-4 lg:p-16">
+      Long ago, Inazuma had five legendary poets, people bestowed upon them the
+      title of The Five Kasen
+    </p>
+  </div>
 );
 
 export default KasenProlog;
