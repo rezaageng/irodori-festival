b5b71ae29c2b6cc4c48617747ac6cdc645377d1a
Reza Ageng <rezaageng5@gmail.com>
2022-05-30 13:21:45 +0700

    feat(api): :sparkles: add api

---
+++ pages/api/index.ts
@@ -0,0 +1,123 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const handler = (_req: NextApiRequest, res: NextApiResponse) => {
+  res.status(200).json([
+    {
+      id: 'iradori1',
+      title: 'iradori1',
+      src: '/assets/images/1.png',
+    },
+    {
+      id: 'iradori2',
+      title: 'iradori2',
+      src: '/assets/images/2.png',
+    },
+    {
+      id: 'iradori3',
+      title: 'iradori3',
+      src: '/assets/images/3.png',
+    },
+    {
+      id: 'iradori4',
+      title: 'iradori4',
+      src: '/assets/images/4.png',
+    },
+    {
+      id: 'iradori5',
+      title: 'iradori5',
+      src: '/assets/images/5.png',
+    },
+    {
+      id: 'iradori6',
+      title: 'iradori6',
+      src: '/assets/images/6.png',
+    },
+    {
+      id: 'iradori7',
+      title: 'iradori7',
+      src: '/assets/images/7.png',
+    },
+    {
+      id: 'iradori8',
+      title: 'iradori8',
+      src: '/assets/images/8.png',
+    },
+    {
+      id: 'iradori9',
+      title: 'iradori9',
+      src: '/assets/images/9.png',
+    },
+    {
+      id: 'iradori10',
+      title: 'iradori10',
+      src: '/assets/images/10.png',
+    },
+    {
+      id: 'iradori11',
+      title: 'iradori11',
+      src: '/assets/images/11.png',
+    },
+    {
+      id: 'iradori12',
+      title: 'iradori12',
+      src: '/assets/images/12.png',
+    },
+    {
+      id: 'iradori13',
+      title: 'irador13',
+      src: '/assets/images/13.png',
+    },
+    {
+      id: 'iradori14',
+      title: 'iradori15',
+      src: '/assets/images/15.png',
+    },
+    {
+      id: 'iradori16',
+      title: 'iradori16',
+      src: '/assets/images/16.png',
+    },
+    {
+      id: 'iradori17',
+      title: 'iradori17',
+      src: '/assets/images/17.png',
+    },
+    {
+      id: 'iradori18',
+      title: 'iradori18',
+      src: '/assets/images/18.png',
+    },
+    {
+      id: 'iradori19',
+      title: 'iradori19',
+      src: '/assets/images/19.png',
+    },
+    {
+      id: 'iradori20',
+      title: 'iradori20',
+      src: '/assets/images/20.png',
+    },
+    {
+      id: 'iradori21',
+      title: 'iradori21',
+      src: '/assets/images/21.png',
+    },
+    {
+      id: 'iradori22',
+      title: 'iradori22',
+      src: '/assets/images/22.png',
+    },
+    {
+      id: 'iradori23',
+      title: 'iradori23',
+      src: '/assets/images/23.png',
+    },
+    {
+      id: 'iradori24',
+      title: 'iradori24',
+      src: '/assets/images/24.png',
+    },
+  ]);
+};
+
+export default handler;
