f9e0d2a2a4fba027c8b66bd515a63732ad25f4df
Reza Ageng <rezaageng5@gmail.com>
2022-06-08 10:18:51 +0700

    feat(gallery): :sparkles: add dynamic transform-origin

--- components/ImageGrid.tsx
+++ components/ImageGrid.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
 import { IImages } from '../types/images';
 
 interface IProps {
@@ -8,6 +9,28 @@ interface IProps {
 
 const ImageGrid = ({ img }: IProps) => {
   const [index, setIndex] = useState<number>(0);
+  const [origin, setOrigin] = useState<string>('');
+
+  const isSm = useMediaQuery({ query: '(min-width: 640px)' });
+  const isLg = useMediaQuery({ query: '(min-width: 1024px)' });
+
+  useEffect(() => {
+    if (isSm) {
+      for (let i = 0; i < img.length; i += 1) {
+        if (index % 3 === 0) setOrigin('origin-left');
+        if (index % 3 === 1) setOrigin('origin-center');
+        if (index % 3 === 2) setOrigin('origin-right');
+      }
+    }
+
+    if (isLg) {
+      for (let i = 0; i < img.length; i += 1) {
+        if (index % 4 === 0) setOrigin('origin-left');
+        if (index % 4 === 1 || index % 4 === 2) setOrigin('origin-center');
+        if (index % 4 === 3) setOrigin('origin-right');
+      }
+    }
+  }, [img, index, isLg, isSm]);
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 lg:gap-4 gap-2">
@@ -15,8 +38,8 @@ const ImageGrid = ({ img }: IProps) => {
         <div
           key={item.id}
           id={item.id}
-          className={`bg-white relative p-2 pb-8 cursor-pointer transition duration-300 hover:delay-300 hover:scale-125 hover:drop-shadow-2xl ${
-            i === index ? 'z-10' : 'z-0'
+          className={`${origin} bg-white relative p-2 pb-8 cursor-pointer transition duration-300 sm:hover:delay-300 sm:hover:scale-125 sm:hover:drop-shadow-2xl ${
+            i === index ? 'z-20' : 'z-0'
           }`}
           onMouseEnter={() => {
             setTimeout(() => {
