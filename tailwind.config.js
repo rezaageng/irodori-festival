385a6aa6144180208923b73facddbb685c721e7a
Reza Ageng <rezaageng5@gmail.com>
2022-06-11 14:09:03 +0700

    feat(config): :sparkles: add color irodori quaternary

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
         'irodori-primary': '#F2E8C9',
         'irodori-secondary': '#f0e2b9',
         'irodori-tertiary': '#b68f3b',
+        'irodori-quaternary': '#A64E46',
       },
       boxShadow: {
         up: '0 -1px 3px 0 rgb(0 0 0 / 0.1), 0 -1px 2px -1px rgb(0 0 0 / 0.1);',
