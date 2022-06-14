98fede6cc474c2bfc5c256bc9ebcaa456f201d06
Reza Ageng <rezaageng5@gmail.com>
2022-06-14 18:29:49 +0700

    feat(meta): :mag: move viewport meta to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,19 +3,25 @@ import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import { ParallaxProvider } from 'react-scroll-parallax';
 import NextNProgress from 'nextjs-progressbar';
+import Head from 'next/head';
 import Layout from '../components/Layout';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <ParallaxProvider>
-    <Layout>
-      <NextNProgress color="#b68f3b" options={{ showSpinner: false }} />
-      <Navbar />
-      <Component {...pageProps} />
-      <Footer />
-    </Layout>
-  </ParallaxProvider>
+  <>
+    <Head>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <ParallaxProvider>
+      <Layout>
+        <NextNProgress color="#b68f3b" options={{ showSpinner: false }} />
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+      </Layout>
+    </ParallaxProvider>
+  </>
 );
 
 export default MyApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,6 @@ import { Html, Head, Main, NextScript } from 'next/document';
 const Document = () => (
   <Html>
     <Head>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta
         name="description"
         content="The Irodori Festival originated from the legend of The Five Kasen who were gifted singers and poetry writers of ancient Inazuma. The Five Kasen brought a cultural revolution to the nation. As a sign of the people's appreciation towards the works of the The Five Kasen, the Irodori Festival was developed."
